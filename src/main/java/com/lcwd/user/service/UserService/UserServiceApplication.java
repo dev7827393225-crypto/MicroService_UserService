84467be75d946d2f08296ace01dd2ab65bbc29cd
Dev Sengar <premlatasengar31@gmail.com>
2026-05-24 18:35:08 +0530

    adding api gateway

--- src/main/java/com/lcwd/user/service/UserService/UserServiceApplication.java
+++ src/main/java/com/lcwd/user/service/UserService/UserServiceApplication.java
@@ -4,14 +4,17 @@ import jakarta.persistence.Entity;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
+@EnableFeignClients
 public class UserServiceApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
